cc90d6bbc223645d3006a6931b9a75599089f633
Evgeniy Falin <share.abromoon@gmail.com>
2025-04-25 13:26:31 +0300

    feat: Add multiple improvements
    - Add header and fix flex containers
    - Add version number
    - Fix sidebar
    - Add icons from Font Awesome
    - Remove unused HelloWorld.vue
    - Update submodules files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 //import { Router } from "vue-router";
+import { FontAwesomeIcon } from './fontawesome.ts'
 import './style.css'
 import router from './router.ts'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).component('FontAwesomeIcon', FontAwesomeIcon).mount('#app')
