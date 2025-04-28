371b857cb8b5002f1651ef11ff7afb55e5d1dd06
Evgeniy Falin <share.abromoon@gmail.com>
2025-04-28 18:51:00 +0300

    feat: Add '@' alias to app src/ directory

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, 'src'),
       'lightweight-charts': path.resolve(__dirname, 'vendor/lw-charts/src/index.ts'),
     }
   }
