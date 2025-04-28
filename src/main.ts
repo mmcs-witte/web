f16679cd63d1976a834de2040cff7d4f5f736393
Evgeniy Falin <share.abromoon@gmail.com>
2025-04-28 18:52:32 +0300

    feature: Add pinia and change app version

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
-//import { Router } from "vue-router";
 import { FontAwesomeIcon } from './fontawesome.ts'
 import './style.css'
 import router from './router.ts'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).use(router).component('FontAwesomeIcon', FontAwesomeIcon).mount('#app')
+const pinia = createPinia()
+
+const app = createApp(App)
+
+app.use(router)
+app.use(pinia)
+app.component('FontAwesomeIcon', FontAwesomeIcon)
+app.mount('#app')
