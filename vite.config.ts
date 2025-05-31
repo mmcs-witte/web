452353b0c741d283b3d3f6f3e2846d1e5e25f788
Iliia Denisov <robbinnegood@gmail.com>
2025-05-31 13:46:29 +0300

    Removed unused code

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      //'lightweight-charts': path.resolve(__dirname, 'vendor/lw-charts/src'),
     }
   }
 })
